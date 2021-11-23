7f598f3029d45a751ab78be76b49ccf989e75365
LEGIONARIO <94655071+Lilpeepdemonx@users.noreply.github.com>
2021-11-23 08:16:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,3 +31,7 @@ apt-get install nodejs
 apt-get install libwebp
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
+ 
+echo "TODO HA SIDO INSTALADO, USA npm start PARA INICIAR EL BOT"
+
+
