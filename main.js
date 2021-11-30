212d60ec69a1b3c1e125f242a9d4150df34e546c
LEGIONARIO <94655071+Lilpeepdemonx@users.noreply.github.com>
2021-11-29 22:49:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ const client = conn.client
 
 
 //Lib
+const config = JSON.parse(fs.readFileSync("./config.json"))
 const owner = config.owner
   const mods = config.mods
   const fake = 'LEGIONARIO'
